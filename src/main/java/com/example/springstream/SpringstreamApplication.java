a01fb513feb499e0dc5088a78c5a6ea0b70e44e4
Gurdial Singh <gurdial.singh@gmail.com>
2020-05-26 08:25:44 +1000

    initial commit

---
+++ src/main/java/com/example/springstream/SpringstreamApplication.java
@@ -0,0 +1,16 @@
+package com.example.springstream;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class SpringstreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringstreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springstream/SpringstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
